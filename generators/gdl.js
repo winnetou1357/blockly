354e325f69cb5b99fdd717d5419c1ceb723e19fb
Mai <Csmai@graphisoft.com>
2019-02-28 10:57:17 +0100

    math compare block felvétele

--- generators/gdl.js
+++ generators/gdl.js
@@ -455,7 +455,38 @@ Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
         "colour": 225,
         "tooltip": "",
         "helpUrl": ""
+    }, {
+        "type": "gdl_math_compare",
+        "message0": "%1 %2 %3",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "A",
+            "check": "Number"
+          },
+          {
+            "type": "field_dropdown",
+            "name": "OP",
+            "options": [
+              ["=", "EQUAL"],
+              ["<", "SMALLER"],
+              [">", "BIGGER"],
+              ["<=", "SMALLEQ"],
+              [">=", "BIGEQ"],
+              ["<>", "NOTEQ"]
+            ]
+          },
+          {
+            "type": "input_value",
+            "name": "B",
+            "check": "Number"
+          }
+        ],
+        "inputsInline": true,
+        "output": "Number",
+        "colour": 190,
     }
+    
 ]);
 
 /**
@@ -490,6 +521,7 @@ Blockly.GDL.ORDER_DIVIDE = Blockly.GDL.ORDER_MULTIPLY;
 Blockly.GDL.ORDER_MODULO = Blockly.GDL.ORDER_MULTIPLY;
 Blockly.GDL.ORDER_ADD = 3;
 Blockly.GDL.ORDER_SUBTRACT = Blockly.GDL.ORDER_ADD;
+Blockly.GDL.ORDER_RELATION = 4;
 
 Blockly.GDL.ORDER_COMMA = 998;          // , parameter separator
 Blockly.GDL.ORDER_NONE = 999;           // 
@@ -652,6 +684,24 @@ Blockly.GDL['math_arithmetic'] = function (block) {
         'DIVIDE': ['/', Blockly.GDL.ORDER_DIVIDE],
         'MODULO': ['%', Blockly.GDL.ORDER_MODULO],
         'POWER': ['^', Blockly.GDL.ORDER_POWER]
+    };
+
+    var [op, order] = OPERATORS[block.getFieldValue('OP')];
+    var argA = Blockly.GDL._numValueToCode(block, 'A', order);
+    var argB = Blockly.GDL._numValueToCode(block, 'B', order);
+
+    var code = argA + Blockly.GDL.CODE_SPACE + op + Blockly.GDL.CODE_SPACE + argB;
+    return [code, order];
+};
+
+Blockly.GDL['gdl_math_compare'] = function (block) {
+    var OPERATORS = {
+        'EQUAL': ['=', Blockly.GDL.ORDER_RELATION],
+        'SMALLER': ['<', Blockly.GDL.ORDER_RELATION],
+        'BIGGER': ['>', Blockly.GDL.ORDER_RELATION],
+        'SMALLEQ': ['<=', Blockly.GDL.ORDER_RELATION],
+        'BIGEQ': ['>=', Blockly.GDL.ORDER_RELATION],
+        'NOTEQ': ['<>', Blockly.GDL.ORDER_RELATION]
     };
 
     var [op, order] = OPERATORS[block.getFieldValue('OP')];
